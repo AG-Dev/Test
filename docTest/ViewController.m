f772eb9efe2ef43480f79fff80aa6ca49e45f6fc
Andrey Golovin <Andrey@MacBook-Pro.local>
2014-01-20 09:30:14 +0200

    etst

--- docTest/ViewController.m
+++ docTest/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ViewController
@@ -17,9 +18,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _webView.delegate = self;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"docx"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:request];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    NSString *document = [webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.innerText"];
+    NSLog(@"%@",document);
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
